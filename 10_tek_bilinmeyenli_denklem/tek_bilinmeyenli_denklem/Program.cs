52db976b24dd717a613f8e25dd881a36922308f8
OsmanFrat <69113898+OsmanFrat@users.noreply.github.com>
2024-02-24 01:47:05 +0300

    10

---
+++ 10_tek_bilinmeyenli_denklem/tek_bilinmeyenli_denklem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tek_bilinmeyenli_denklem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, x;
+
+            Console.WriteLine("a değenini giriniz: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("b değenini giriniz: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            x = -b / a;
+
+            Console.Write("x = " + x);
+
+            Console.ReadLine();
+        }
+    }
+}
