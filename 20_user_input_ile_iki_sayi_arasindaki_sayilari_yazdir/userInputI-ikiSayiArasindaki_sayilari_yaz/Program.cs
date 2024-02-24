31b455a34b6a784ac04e2073c50a1c29012dc1ad
OsmanFrat <69113898+OsmanFrat@users.noreply.github.com>
2024-02-24 21:40:20 +0300

    20

---
+++ 20_user_input_ile_iki_sayi_arasindaki_sayilari_yazdir/userInputI-ikiSayiArasindaki_sayilari_yaz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baslangic, bitis, artis = 1;
+
+            Console.WriteLine("Baslangıç: ");
+            baslangic = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bitiş: ");
+            bitis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Artiş: ");
+            artis = Convert.ToInt32(Console.ReadLine());
+
+            // ; ile başlamasının sebebi doğrudan döngünün başlangıç değerinden başlatmak istediğimiz için.
+            for (; baslangic <= bitis; baslangic += artis)
+            {
+                Console.WriteLine(baslangic);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
