1a08c694dba2f1cc5103be81ea2b029f8fb3efac
OsmanFrat <69113898+OsmanFrat@users.noreply.github.com>
2024-02-24 17:04:19 +0300

    18-19

---
+++ 18_baglantisiz_ic_ice_for_dongusu/baglantisiz_ic_ice_for_dongusu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baglantisiz_ic_ice_for_dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            for(b = 1; b <= 5; b++)
+            {
+                /* Üstteki döngü bir kez çalıştığında, bu döngü 5 defa, yani bitene kadar çalışır
+                *  ve tekrar üstteki döngüye dönülür. Ardından üstteki döngüde b değeri 2 olur ve tekrar
+                *  altaki döngüye dönülür. Aynı işlem b değeri 5'i geçinceye kadar devam eder.*/  
+                for (a = 1; a <= 5; a++)
+                {
+                    Console.WriteLine("b = " + b + "\t a = " + a); 
+                }
+                Console.WriteLine("---------------");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 19_baglantili_ic_ice_for_dongusu/baglantili_ic_ice_for_dongusu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baglantili_ic_ice_for_dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                /* İlk döngü bir defa çalışır ve alta inilir.
+                 * İkinci döngü j değeri i değerine ulaşıncaya kadar çalışır. 
+                 * j değeri i'yi geçince döngü biter ve üstteki döngüye geçilir.
+                 * i değeri artık 2 'dir ve tekrar çalışır.
+                 * İkinci döngüdeki j değeri tekrar 1 olmuştur. Çünkü içteki 
+                 * döngü her tamamlandığında sıfırlanır.
+                 * Bu sefer i değeri 2 oluğu için ekranda ikinci satıra 12 yazar ve
+                 * tekrar döngü tamamlandığı için sıfırlanır ve üstteki döngüye dönülür.
+                 * Bu işlemler üstteki döngüdeki i değeri 10'u geçinceye kadar tekrarlanır.*/
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);// .Write() metodunu kullanmamızın sebebi, sayıları yan yana yazmak istememiz.
+                }
+                Console.WriteLine("");// Her döngüler tamamlandığında yeni satıra geçmek için kullanılır.
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
